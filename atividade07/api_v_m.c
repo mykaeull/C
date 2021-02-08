e35ffba9b155ad971dce044af465f23717ff9db5
Myka <mykaeull99@gmail.com>
2021-02-08 19:32:04 -0200

    [-] Atividade 07 finalizada

--- atividade07/api_v_m.c
+++ atividade07/api_v_m.c
@@ -5,53 +5,44 @@
 int main() {
     void *vetor;
     void *matriz;
+
+    vetor = criar_vetor(16);
+	atribuir_vetor(vetor, 8, 456.0);
+	printf("A pos. 8 do vetor contem: %f.\n", obter_vetor(vetor, 8));
+	liberar_vetor(vetor);
+
+	matriz = criar_matriz(10, 10);
+	atribuir_matriz(matriz, 5, 4, 12.0);
+	printf("A pos. (5,4) da matriz contem: %f.\n", obter_matriz(matriz, 5, 4));
+	liberar_matriz(matriz);
     
+    /* ===== CÓDIGO PARA TESTE DA API =====
     vetor = criar_vetor(4);
-    printf("%d\n", atribuir_vetor(vetor, 0, 1.0));
-    printf("%d\n", atribuir_vetor(vetor, 1, 2.0));
-    printf("%d\n", atribuir_vetor(vetor, 2, 3.0));
-    printf("%d\n", atribuir_vetor(vetor, 3, 4.0));
-    printf("%lf\n", obter_vetor(vetor, 0));
-    printf("%lf\n", obter_vetor(vetor, 1));
-    printf("%lf\n", obter_vetor(vetor, 2));
-    printf("%lf\n", obter_vetor(vetor, 3));
+    printf("%d\n", atribuir_vetor(vetor, 0, 1.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_vetor(vetor, 1, 2.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_vetor(vetor, 2, 3.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_vetor(vetor, 3, 4.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%lf\n", obter_vetor(vetor, 0)); // caso 0: falha na operação
+    printf("%lf\n", obter_vetor(vetor, 1)); // caso 0: falha na operação
+    printf("%lf\n", obter_vetor(vetor, 2)); // caso 0: falha na operação
+    printf("%lf\n", obter_vetor(vetor, 3)); // caso 0: falha na operação
     liberar_vetor(vetor);
 
     matriz = criar_matriz(3, 2);
-    printf("%d\n", atribuir_matriz(matriz, 0, 0, 1.0));
-    printf("%d\n", atribuir_matriz(matriz, 0, 1, 2.0));
-    printf("%d\n", atribuir_matriz(matriz, 1, 0, 3.0));
-    printf("%d\n", atribuir_matriz(matriz, 1, 1, 4.0));
-    printf("%d\n", atribuir_matriz(matriz, 2, 0, 5.0));
-    printf("%d\n", atribuir_matriz(matriz, 2, 1, 6.0));
-    printf("%lf\n", obter_matriz(matriz, 0, 0));
-    printf("%lf\n", obter_matriz(matriz, 0, 1));
-    printf("%lf\n", obter_matriz(matriz, 1, 0));
-    printf("%lf\n", obter_matriz(matriz, 1, 1));
-    printf("%lf\n", obter_matriz(matriz, 2, 0));
-    printf("%lf\n", obter_matriz(matriz, 2, 1));
+    printf("%d\n", atribuir_matriz(matriz, 0, 0, 1.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_matriz(matriz, 0, 1, 2.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_matriz(matriz, 1, 0, 3.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_matriz(matriz, 1, 1, 4.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_matriz(matriz, 2, 0, 5.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%d\n", atribuir_matriz(matriz, 2, 1, 6.0)); // caso 0: falha na operação | caso 1: sucesso na operação
+    printf("%lf\n", obter_matriz(matriz, 0, 0)); // caso 0: falha na operação
+    printf("%lf\n", obter_matriz(matriz, 0, 1)); // caso 0: falha na operação
+    printf("%lf\n", obter_matriz(matriz, 1, 0)); // caso 0: falha na operação
+    printf("%lf\n", obter_matriz(matriz, 1, 1)); // caso 0: falha na operação
+    printf("%lf\n", obter_matriz(matriz, 2, 0)); // caso 0: falha na operação
+    printf("%lf\n", obter_matriz(matriz, 2, 1)); // caso 0: falha na operação
     liberar_matriz(matriz);
-
-    
-    
-    
-
-    /*vetor = criar_vetor(tam);
-    vetor[0] = 2.0;
-    atribuir_vetor(vetor, 1, 3.0);
-    atribuir_vetor(vetor, 2, 4.0);
-    atribuir_vetor(vetor, 3, 5.0);
-    printf("%d\n", vetor);
-    printf("%lf\n", vetor[0]);
-    printf("%lf\n", vetor[1]);
-    liberar_vetor(vetor, tam);
-    printf("%d\n", vetor);
-    printf("%lf\n", vetor[0]);
-    printf("%lf\n", vetor[1]);
-    printf("%lf\n", vetor[2]);
-    printf("%lf\n", vetor[3]); */
-
-    // printf("%d", *(vetor+1)); mostra a proxima posicao
+    */
 
     return 0;
 }
--- atividade07/dinamica.c
+++ atividade07/dinamica.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void* criar_vetor(int tam) {
-    double *vetor = (double*)malloc((tam+1)*sizeof(double));
+    double *vetor = (double*)malloc((tam+1)*sizeof(double)); // 0 1 2 3 4 5
     ((int*)vetor)[0] = tam; // guarda o tamanho do vetor para futuras operações
     return vetor;
 }
@@ -11,13 +11,13 @@ double obter_vetor(void *vetor, int x) {
     int tam = ((int*)vetor)[0];
     int p = x + 1;
     if (x < 0 || x >= tam) {
-        return -1; // posição inválida
+        return 0; // posição inválida
     } else {
         return ((double*)vetor)[p];
     }
 }
 
-int atribuir_vetor(void *vetor, int x, double val) {
+int atribuir_vetor(void *vetor, int x, double val) { 
     int tam = ((int*)vetor)[0];
     int p = x + 1; // como a posição 0 do vetor ja possui um elemento, todo novo elemento será armazenado no próximo endereço de memória
     if (x < 0 || x >= tam) {
@@ -43,7 +43,7 @@ double obter_matriz(void *matriz, int l, int c) {
     int num_l = ((int*)matriz)[0];
     int num_c = ((int*)matriz)[1];
     if ((l < 0 || l >= num_l) || (c < 0 || c >= num_c)) {
-        return -1; // posição inválida
+        return 0; // posição inválida
     } else {
         return *(((double*)matriz+2) + l * (num_l - 1) + c);
     }
