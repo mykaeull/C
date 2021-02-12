5b4899459dc5f4e79f496c00a332db67e5e0d9a4
Myka <mykaeull99@gmail.com>
2021-02-12 20:42:45 -0200

    [-] Funcao de receber arquivo finalizada

--- atividade08/num_ext.c
+++ atividade08/num_ext.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
@@ -45,61 +44,127 @@ void preencher_arr() {
 	arr[900] = "novecentos";
 }
 
+int str_tamanho(char *s) {
+    int tam = 0;
+    while(s[tam]) {
+        tam ++;
+    }
+    return tam;
+}
+
+void str_copia(char *s, char *d) {
+    int i = 0;
+    while(s[i] != '\0') { 
+        d[i] = s[i]; 
+        i++;
+    }
+    d[i] = '\0';
+}
+
+void str_concatena(char *s, char *d) {
+    int i = 0, j = 0;
+    while(d[i] != '\0') {
+        i++; 
+    }
+    while(s[j] != '\0') { 
+        d[i] = s[j]; 
+        i++;
+        j++;
+    }
+    d[i] = '\0';
+}
+
 char* num_ext(int n) {
     int num_dig = floor(log10(n)) + 1;
     char *concat = (char*)malloc(sizeof(char)*100);
     int unidade, dezena, centena;
     
-    if (num_dig == 1) {
-        sprintf(concat, arr[n]);
-        printf("%s", concat);
-    } else if (num_dig == 2) {
-        if (n > 9 && n < 20) {
-            sprintf(concat, arr[n]);
-            printf("%s", concat);
-        } else {
-            unidade = n % 10;
-            dezena = n - unidade;
-            sprintf(concat, "%s %s %s", arr[dezena], "e", arr[unidade]);
-            printf("%s", concat);
-            printf("\n%d", strlen(concat));
-        }
+    if (n == 0) {
+        printf("-1");
     } else {
-        if (n == 100) {
-            sprintf(concat, arr[0]);
-            printf("%s", concat);
-        } else {                       // 113
-            centena = (n / 100) * 100; // 100
-            unidade = n % 10;          // 2
-            dezena = (n - centena) - unidade; // 10
-            if (unidade == 0) {
-                sprintf(concat, "%s %s %s", arr[centena], "e", arr[dezena]);
-                printf("%s", concat);
-            } else if (dezena == 10) {
-                sprintf(concat, "%s %s %s", arr[centena], "e", arr[dezena+unidade]);
-                printf("%s", concat);
+        if (num_dig == 1) {
+            sprintf(concat, arr[n]);
+        } else if (num_dig == 2) {
+            if (n > 9 && n < 20) {
+                sprintf(concat, arr[n]);
             } else {
-                sprintf(concat, "%s %s %s %s %s", arr[centena], "e", arr[dezena], "e", arr[unidade]);
-                printf("%s", concat);
+                unidade = n % 10;
+                dezena = n - unidade;
+                if (unidade == 0){
+                    sprintf(concat, arr[dezena]);
+                } else {
+                    sprintf(concat, "%s %s %s", arr[dezena], "e", arr[unidade]);
+                }
+            }
+        } else {
+            if (n == 100) {
+                sprintf(concat, arr[0]);
+            } else {                       
+                centena = (n / 100) * 100; 
+                unidade = n % 10;          
+                dezena = (n - centena) - unidade; 
+                if (unidade == 0) {
+                    sprintf(concat, "%s %s %s", arr[centena], "e", arr[dezena]);
+                } else if (dezena == 10) {
+                    sprintf(concat, "%s %s %s", arr[centena], "e", arr[dezena+unidade]);
+                } else {
+                    sprintf(concat, "%s %s %s %s %s", arr[centena], "e", arr[dezena], "e", arr[unidade]);
+                }
             }
         }
     }
     return concat;
 }
 
-int main() {
-    int n;
-    int t = 0;
+int main(int argc, char *argv[]) {
+    int n, x, primeiro = 0;
     char *c;
-    scanf("%d", &n);
+    char numextenso[1024];
+    int tam;
+
     preencher_arr();
 
-    c = num_ext(n);
+    if (argc == 1) {
+        printf("entre um numero entre 1 e 999: ");
+        scanf("%d", &n);
 
-    while (c[t]) {
-        t++;
+        c = num_ext(n);
+        tam = str_tamanho(c);
+        str_copia(c, numextenso);
+        printf("%s\n", numextenso);
+        printf("tamanho da string: %d", tam);
+
+        // printf("\ntamanho da string: %d", tam);
+    } else {
+        FILE *arq;
+        char num[1000];
+        arq = fopen(argv[1], "r");
+        if (arq == NULL) {
+            printf("nao foi possivel encontrar o arquivo.");
+            return EXIT_FAILURE;
+        }
+        while(fgets(num, 5, arq) != NULL) {
+            x = atoi(num); 
+            c = num_ext(x); 
+            printf("%d\n", x);
+
+            if (primeiro == 0) {
+                str_copia(c, numextenso);
+                printf("%s\n", c);
+                printf("tamanho da string: %d\n", str_tamanho(c));
+                printf("strings concatenadas: %s\n", numextenso);
+            } else {
+                printf("%s\n", c);
+                printf("tamanho da string: %d\n", str_tamanho(c));
+                str_concatena("| ", numextenso);
+                str_concatena(c, numextenso);
+                printf("strings concatenadas: %s\n", numextenso);
+            }
+            primeiro = 1;
+            printf("\n");
+        }
+        fclose(arq);
     }
-    printf("\n%d", t);
 
     return 0;
 }
